06a906439be0d06f0af783b7ad4a8011670814e7
Aishwarya Prakash <aishwaryaaprakash88@gmail.com>
2026-03-18 09:47:56 +0530

    Update palindromecheckerapp.java

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,26 +1,19 @@
 import java.util.Scanner;
-import java.util.Stack;
 
-public class palindromecheckerapp {
+public class PalindromeUC9 {
+
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) return true;
+        if (str.charAt(start) != str.charAt(end)) return false;
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
-        }
-
-        boolean isPalindrome = true;
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) != stack.pop()) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome) {
+        if (isPalindrome(input, 0, input.length() - 1)) {
             System.out.println("The string is a palindrome.");
         } else {
             System.out.println("The string is not a palindrome.");
